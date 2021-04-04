de7fbdbee063958e300ccc4cecec21ebf33496cf
Your Name <oleksii.dolhov@gmail.com>
2021-04-05 01:15:07 +0300

    changed to nullpull in db

--- handler/session_maker.py
+++ handler/session_maker.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 from .config import Config
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
@@ -11,7 +12,8 @@ HOST = Config.HOST
 SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DB}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    poolclass=NullPool
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
